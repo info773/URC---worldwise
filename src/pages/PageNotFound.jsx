5b103715c35818cff4258c87392c717e72a0061c
“Berry” <info@felix-behrens.de>
2026-03-01 15:02:52 +0100

    initial commit

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <p>404 Not found :(</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
